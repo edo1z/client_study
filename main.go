48b8fd29b3e9e7cd0b16b07d76cb553df00fa1dd
edo1z <edo1z3145@gmail.com>
2017-06-14 00:00:15 +0900

    client for return daytime server

--- main.go
+++ main.go
@@ -8,19 +8,12 @@ import (
 
 func main() {
 	host := "127.0.0.1:7777"
-	msg := "hello"
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", host)
 	chkErr(err, "tcpAddr")
-	fmt.Println(tcpAddr)
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	chkErr(err, "DialTCP")
-	fmt.Println(conn)
-
-	_, err = conn.Write([]byte(msg))
-	chkErr(err, "Write")
-	fmt.Println("Wrote")
 
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
