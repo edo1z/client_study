53a4a150571d6e56f43e5dabb79448daeeee53f4
edo1z <edo1z3145@gmail.com>
2017-06-14 19:53:07 +0900

    client file for chat

---
+++ client_for_daytime.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func daytime() {
+	host := "127.0.0.1:7777"
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", host)
+	ChkErr(err, "tcpAddr")
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	ChkErr(err, "DialTCP")
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	ChkErr(err, "Read")
+	fmt.Println(string(buf[:n]))
+}
--- main.go
+++ main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net"
-	"os"
-)
+import ()
 
 func main() {
-	host := "127.0.0.1:7777"
-
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", host)
-	chkErr(err, "tcpAddr")
-
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	chkErr(err, "DialTCP")
-
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	chkErr(err, "Read")
-	fmt.Println(string(buf[:n]))
-}
-
-func chkErr(err error, place string) {
-	if err != nil {
-		fmt.Printf("(%s)", place)
-		fmt.Fprintf(os.Stderr, "%s", err.Error())
-		os.Exit(0)
-	}
+	Chat()
 }
